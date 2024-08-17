6ad785e6178a34b769d273161c55b293ca9a2e68
case-E <kc@schooltendance.com>
2024-08-17 20:30:08 +0530

    initial config and getting started doc

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # schooltendance-help-docs
 Help Docs for Schooltendance
 
+### Contributing
+- Start Here: The start here section should have all the info related to "Getting Started" and/or why someone should choose our product over others. The comparison page shouldn't be marketing jargon but more of a feature list on our end/value add to the users. 
+- Guides lead a user through a specific task they want to accomplish, often with a sequence of steps. Writing a good guide requires thinking about what your users are trying to do. Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework.
+- Reference pages are ideal for outlining how things work in terse and clear terms.
+Less concerned with telling a story or addressing a specific use case, they should give a comprehensive outline of what you're documenting. For example, it could be information about all the fields that can be configured in the admissions form and or API reference. Read [about reference](https://diataxis.fr/reference/) in the Diátaxis framework.
+- Troubleshooting pages help users solve common problems they might encounter. They should be written in a way that helps users diagnose and solve their own problems. For example, it could be a page that explains how to troubleshoot a failed payment or how to troubleshoot a failed API request.
+- [Frontmatter Reference](https://starlight.astro.build/reference/frontmatter/)
+
 ## Getting Started
 
 | Command                   | Action                                           |
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,63 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Schooltendance Help Centre",
+      logo: {
+        src: "./src/assets/helpcentre-logo.svg",
+        replacesTitle: true,
+      },
+      social: {
+        linkedin: "https://linkedin.com/schooltendance",
+        instagram: "https://instagram.com/schooltendance",
+        "x.com": "https://x.com/schooltendance",
+        email: "mailto:support@schooltendance.com",
+        github: "https://github.com/schooltendance",
+      },
+      head: [
+        {
+          tag: "script",
+          content:
+            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PJFH35KK');",
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      components: {
+        SkipLink: "./src/components/SkipLink.astro", // override the default SkipLink component to load the GTM script in the body
+        Footer: "./src/components/ConditionalFooter.astro",
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: {
+            directory: "start_here",
+          },
+        },
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+      editLink: {
+        baseUrl:
+          "https://github.com/schooltendance/schooltendance-help-docs/edit/main",
+      },
+      pagination: true,
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
