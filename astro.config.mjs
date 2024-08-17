053d2e2842bef8bc984b487741d1dfe678a66759
case-E <kc@schooltendance.com>
2024-08-17 21:15:57 +0530

    deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://help.schooltendance.com",
   integrations: [
     starlight({
       title: "Schooltendance Help Centre",
